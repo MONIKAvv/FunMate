1157ef458868515be6ea81f31638709d1875d7b7
MONIKAvv <monikalonewolf35@gmail.com>
2025-08-13 13:42:27 +0530

    checkin

--- app/src/main/java/vv/monika/funmate/MainActivity.kt
+++ app/src/main/java/vv/monika/funmate/MainActivity.kt
@@ -1,7 +1,10 @@
 package vv.monika.funmate
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Message
+import android.system.Os.close
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -23,8 +26,13 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import vv.monika.funmate.databinding.ActivityMainBinding
+import vv.monika.funmate.databinding.AppBlockedBinding
+import vv.monika.funmate.databinding.MaintanenceAlertBinding
 import vv.monika.funmate.databinding.SideNavigationBarBinding
+import vv.monika.funmate.databinding.UpdateActivityBinding
+import vv.monika.funmate.databinding.VpnDetectedBinding
 import vv.monika.funmate.fragment.HomeFragment
 import vv.monika.funmate.fragment.ListFragment
 import vv.monika.funmate.fragment.RecentFragment
@@ -34,6 +42,7 @@ import vv.monika.funmate.ui.theme.FunMateTheme
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navigationView: NavigationView
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +52,53 @@ class MainActivity : AppCompatActivity() {
 
         drawerLayout = binding.drawerLayout
         bottomNavigationView = binding.bottomNavigationView
+
+      binding.sideNavView.setNavigationItemSelectedListener { menuItem ->
+          when( menuItem.itemId) {
+              R.id.nav_home -> {
+                  supportFragmentManager.beginTransaction()
+                      .replace(R.id.fragment_view, HomeFragment())
+                      .commit()
+
+              }
+              R.id.nav_setting -> {
+                  Toast.makeText(this, "Setting Clicked", Toast.LENGTH_SHORT).show()
+                  true
+              } R.id.nav_redeem -> {
+             startActivity(Intent(this, RadeemCodeActivity::class.java))
+              }
+              R.id.nav_history -> {
+                  supportFragmentManager.beginTransaction()
+                      .replace(R.id.fragment_view, RecentFragment())
+                      .commit()
+              } R.id.nav_telegram -> {
+              Toast.makeText(this, "telegram Clicked", Toast.LENGTH_SHORT).show()
+                  true
+              }
+              R.id.nav_rating -> {
+                  Toast.makeText(this, "Rating Clicked", Toast.LENGTH_SHORT).show()
+                  true
+              } R.id.nav_share -> {
+             startActivity(Intent(this, InviteActivity::class.java))
+              }
+              R.id.nav_email -> {
+                  Toast.makeText(this, "Email Clicked", Toast.LENGTH_SHORT).show()
+                  true
+              } R.id.nav_delete_acc -> {
+              Toast.makeText(this, "Delete Clicked", Toast.LENGTH_SHORT).show()
+              true
+          }R.id.nav_logout -> {
+              Toast.makeText(this, "Logout Clicked", Toast.LENGTH_SHORT).show()
+              true
+          }
+              else -> false
+      }
+          drawerLayout.closeDrawer(GravityCompat.START)
+          true
+
+      }
+
+
 
 
 //        show shome fragment by default
